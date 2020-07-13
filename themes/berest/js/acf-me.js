9eaa07d6ed3fc2e0cddb1b9291b2c4c7a7f505ae
Ajiewa <Ajiewa@WIN-63UDTOLA7M5>
2020-07-13 14:50:16 +0300

    added onload fields initialisation

--- themes/berest/js/acf-me.js
+++ themes/berest/js/acf-me.js
@@ -18,3 +18,19 @@ acf.addAction('hide_field/type=text', function( field ){
         jQuery('#image-gallery-meta-box').css('display', 'none');
 
 });
+
+// show "Rates" / "Model Image" if they are selected on page load
+acf.addAction('load_field/name=rates_tab', onRatesLoad);
+
+function onRatesLoad(field) {
+    if (field.isActive()) {
+        jQuery('#advanced-sortables').css('display', 'block');
+    }
+}
+
+acf.addAction('load_field/name=image_tab', onModelImageLoad);
+function onModelImageLoad(field) {
+    if (field.isActive()) {
+        jQuery('#image-gallery-meta-box').css('display', 'block');
+    }
+}
